d86f28d9a32bf23ba9397f557c8bec4618da57d6
Mateus Monteiro Ramos <devteus.officeti@gmail.com>
2026-01-11 18:50:56 -0300

    Arrumando Router

--- mercado/src/main.jsx
+++ mercado/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Mercado'>
+    <BrowserRouter basename='/MercadoClone'>
     <Routes>
       <Route path='/*' element={<Home/>}/>
     </Routes>
